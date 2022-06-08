06f6d2c2509522b45d5c877af705b1be03137d72
Sylvain Hellegouarch <sh@defuze.org>
2022-06-08 18:31:12 +0200

    Bump version 0.2.1 -> 0.3.0
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- chaosgcp/__init__.py
+++ chaosgcp/__init__.py
@@ -31,7 +31,7 @@ __all__ = [
     "wait_on_operation",
     "load_credentials",
 ]
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 def get_service(
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="chaostoolkit-google-cloud-platform",
-        version="0.2.1"
+        version="0.3.0"
     )
