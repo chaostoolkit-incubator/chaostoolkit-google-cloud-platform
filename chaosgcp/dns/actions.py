369f5456d13ca40f221d8b42c5503ef4d2e25805
Lalitha Madhurima Gayathri Kolluru <lkolluru@google.com>
2023-12-13 15:45:08 -0800

    testes updated

--- chaosgcp/dns/actions.py
+++ chaosgcp/dns/actions.py
@@ -1,35 +1,33 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from chaoslib.types import Configuration, Secrets
-from chaosgcp import client
 
-from chaosgcp import load_credentials
+from chaosgcp import client
 
 __all__ = ["update_service"]
 
 
 def update_service(
-    secrets: Secrets = None,
     project: str = None,
-    ip_address: str= None,
-    name: str= None,
-    zone_name: str= None,
-    kind: str= "dns#resourceRecordSet",
-    ttl: int=5,
-    record_type: str="A",
-    existing_type: str="A"):
-
-    credentials = load_credentials(secrets)
-
-    service = client('dns', 'v1', credentials=credentials) 
+    ip_address: str = None,
+    name: str = None,
+    zone_name: str = None,
+    kind: str = "dns#resourceRecordSet",
+    ttl: int = 5,
+    record_type: str = "A",
+    existing_type: str = "A",
+    configuration: Configuration = None,
+    secrets: Secrets = None,
+) -> Dict[str, Any]:
+    service = client("dns", "v1", secrets=secrets)
 
     dns_record_body = {
-        'kind': kind,   
-        'name': name,   
-        'rrdatas': [ip_address],      
-        'ttl': ttl,  
-        'type': record_type
+        "kind": kind,
+        "name": name,
+        "rrdatas": [ip_address],
+        "ttl": ttl,
+        "type": record_type,
     }
 
     request = service.resourceRecordSets().patch(
@@ -37,8 +35,8 @@ def update_service(
         managedZone=zone_name,
         name=name,
         type=existing_type,
-        body=dns_record_body
-        )
+        body=dns_record_body,
+    )
 
     response = request.execute()
 
