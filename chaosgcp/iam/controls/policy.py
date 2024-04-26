81b2d9f8e2204a142f5b2188f8d0650d76e9d117
Ashish Jain <ashishjn@google.com>
2024-04-26 12:41:57 +0530

    Reformatted after PDM format
    Signed-off-by: Ashish Jain <ashishjn@google.com>

--- chaosgcp/iam/controls/policy.py
+++ chaosgcp/iam/controls/policy.py
@@ -1,16 +1,16 @@
-#Copyright 2024 Google LLC
+# Copyright 2024 Google LLC
 
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #    https://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from typing import List
 from chaoslib.exceptions import InterruptExecution
@@ -302,4 +302,3 @@ def modify_policy_remove_roles(
         if binding is not None:
             policy["bindings"].remove(binding)
     set_policy(crm_service, project_id, policy)
-
