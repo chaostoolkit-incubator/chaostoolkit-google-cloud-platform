7ca09cfeac0361d15bb84404e6e1e135319e1a1c
Ashish Jain <ashishjn@google.com>
2024-07-08 15:29:18 +0530

    Updated The AppHub Actions As per the PR comments "
    Signed-off-by: Ashish Jain <ashishjn@google.com>

--- chaosgcp/apphub/actions.py
+++ chaosgcp/apphub/actions.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import logging
-from typing import Dict, Any
+from typing import Dict, Any, List, Generator, Optional
 
 from google.cloud import compute_v1, apphub_v1
 from google.cloud import resourcemanager_v3
@@ -23,6 +23,9 @@ from chaosgcp.lb.actions import (
     inject_traffic_faults,
     remove_fault_injection_traffic_policy,
 )
+from chaosgcp import get_context, load_credentials
+from chaoslib.exceptions import ActivityFailed
+
 
 __all__ = [
     "inject_fault_if_url_map_exists_app_hub",
@@ -31,56 +34,61 @@ __all__ = [
 logger = logging.getLogger("chaostoolkit")
 
 
-def get_project_number(project_id: str):
-    """Retrieves the project number for the specified Google Cloud Project ID."""
-    client = resourcemanager_v3.ProjectsClient()
-    # Construct the project name (required format)
-    project_name = f"projects/{project_id}"
-    try:
-        project = client.get_project(name=project_name)
-        return project.name.split("/")[-1]
-        # Extract number from 'projects/123456789'
-    except Exception as e:
-        logger.error(f"Error retrieving project number: {e}")
-        return None
-
-
-def apphub_app_list_services(project_id: str, location: str, application_id: str):
+def apphub_app_list_services(
+    host_project_id: str,
+    project_id: str,
+    location: str,
+    application_id: str,
+    configuration: Configuration = None,
+    secrets: Secrets = None,
+) -> List[str]:
     """Retrieves a list of service URIs associated with an AppHub application.
 
     This function queries the AppHub API to list all services deployed within
-    a specific application and extracts their associated URIs. It replaces the
-    project number with the project ID in the URI for improved clarity.
+    a specific application and extracts their associated URIs.
 
     Args:
-        project_id (str): The ID of the Google Cloud project containing the application.
+        host_project_id (str): The ID of the Host Apphub project.
+        project_id (str): The ID of the Google Cloud project to inject fault into.
         location (str): The geographic location (e.g., "us-central1") of the application.
         application_id (str): The unique identifier for the AppHub application.
 
     Returns:
         list: A list of strings, where each string is the URI of a service associated
               with the specified application. The URIs are formatted to contain the
-              project ID instead of the project number."""
-    name_app = f"projects/{project_id}/locations/{location}/applications/{application_id}"
-    client = apphub_v1.AppHubClient()
-
-    request = apphub_v1.ListServicesRequest(parent=name_app)
-
-    service_uris = []
-    logger.info("Services for given Application in Apphub")
-    for response in client.list_services(request=request):
-        uri = response.service_reference.uri.replace(
-            get_project_number(project_id), project_id
+                      project ID instead of the project number only for project_id."""
+    try:
+        name_app = f"projects/{host_project_id}/locations/{location}/applications/{application_id}"
+        credentials = load_credentials(secrets)
+        client = apphub_v1.AppHubClient()
+        request = apphub_v1.ListServicesRequest(parent=name_app)
+        service_uris = []
+        updated_uri = None
+        logger.info("Getting attached Services of given Application in Apphub")
+        for response in client.list_services(request=request):
+            uri = response.service_reference.uri.replace(
+                resourcemanager_v3.ProjectsClient(credentials=credentials)
+                .get_project(name="projects/{}".format(project_id))
+                .name,
+                "projects/{}".format(project_id),
+            )
+            updated_uri = (
+                uri[uri.find("/projects/") :] if "/projects/" in uri else uri
+            )
+            service_uris.append(updated_uri)
+        logger.debug(f"Listing Services {updated_uri}")
+        return service_uris
+    except Exception as e:
+        raise ActivityFailed(
+            f"Error Listing Services of Apphub Application: {e}"
         )
-        updated_uri = (
-            uri[uri.find("/projects/") :] if "/projects/" in uri else uri
-        )
-        service_uris.append(updated_uri)
-    logger.info(updated_uri)
-    return service_uris
 
 
-def get_url_map_essentials(project_id: str):
+def get_url_map_essentials(
+    project_id: str,
+    configuration: Configuration = None,
+    secrets: Secrets = None,
+) -> Optional[Generator[Dict[str, str], Any, None]]:
     """Retrieves essential information for URL maps in a Google Cloud project.
 
     This function queries the Google Compute Engine API to fetch key details
@@ -94,49 +102,51 @@ def get_url_map_essentials(project_id: str):
         project_id (str): The ID of the Google Cloud project to query.
 
     Yields:
-        dict: A dictionary containing the region, service, and self_link of each
+        Generator[Dict[str, str]]: A dictionary containing the region, service, and self_link of each
             URL map found in the project.
 
     Raises:
         Error: If there's an error communicating
             with the Google Cloud API (e.g., authentication issues, invalid project ID).
     """
-    client = compute_v1.UrlMapsClient()
-    request = compute_v1.AggregatedListUrlMapsRequest(project=project_id)
+    try:
+        # client = compute_v1.UrlMapsClient()
+        # request = compute_v1.AggregatedListUrlMapsRequest(project=project_id)
+        credentials = load_credentials(secrets)
+        context = get_context(configuration, project_id=project_id)
+        client = compute_v1.UrlMapsClient(credentials=credentials)
+        request = compute_v1.AggregatedListUrlMapsRequest(
+            project=context.project_id
+        )
 
-    for response in client.aggregated_list(request=request):
-        scope, url_maps_scoped_list = response
-        region = scope.split("/")[-1] if "/" in scope else "global"
+        for response in client.aggregated_list(request=request):
+            scope, url_maps_scoped_list = response
+            region = scope.split("/")[-1] if "/" in scope else "global"
 
-        if url_maps_scoped_list.url_maps:
-            for url_map in url_maps_scoped_list.url_maps:
-                yield {
-                    "region": region,
-                    "service": url_map.default_service[
-                        url_map.default_service.find("/projects/") :
-                    ]
-                    if "/projects/" in url_map.default_service
-                    else url_map.default_service,
-                    "self_link": url_map.self_link,
-                }
-
-
-def get_urlMap_self_links_for_matching_services(
-    service_uris, url_map_essentials
-):
-    """Finds self_links of URL maps whose services match the given service URIs."""
-    matching_self_links = []
-    logger.info(
-        "Finds self_links of URL maps whose services match with given services of Application"
-    )
-    for details in url_map_essentials:
-        if details["service"] in service_uris:
-            matching_self_links.append(details["self_link"])
-            logger.info(details["self_link"])
-    return matching_self_links
+            if url_maps_scoped_list.url_maps:
+                for url_map in url_maps_scoped_list.url_maps:
+                    yield {
+                        "region": region,
+                        "service": url_map.default_service[
+                            url_map.default_service.find("/projects/") :
+                        ]
+                        if "/projects/" in url_map.default_service
+                        else url_map.default_service,
+                        "self_link": url_map.self_link,
+                    }
+    except Exception as e:
+        raise ActivityFailed(f"Error getting urlMaps {e}")
 
 
-def url_map_exists(url_map_value: str, project_id: str, region: str, application_name: str):
+def url_map_exists(
+    url_map_value: str,
+    host_project_id: str,
+    project_id: str,
+    region: str,
+    application_name: str,
+    configuration: Configuration = None,
+    secrets: Secrets = None,
+) -> bool:
     """Checks if a specific URL map value exists within AppHub-related URL maps.
 
     This function first retrieves the selfLink URLs of all URL maps associated with services
@@ -145,37 +155,52 @@ def url_map_exists(url_map_value: str, project_id: str, region: str, application
 
     Args:
         url_map_value (str): The specific value (e.g., "my-url-map") to search for after "urlMaps/" in the URL.
-        project_id (str): The ID of the Google Cloud project containing the AppHub application.
+        host_project_id (str): The ID of the Host Apphub project.
+        project_id (str): The ID of the Google Cloud project where service exists.
         region (str): The region where the AppHub application is deployed.
         application_name (str): The name of the AppHub application.
 
     Returns:
         bool: True if the `url_map_value` is found within the URL maps associated with
-              the specified AppHub application, False otherwise.
+              the specified AppHub application.
     """
     try:
-        availableUrlMaps = get_urlMap_self_links_for_matching_services(
-            apphub_app_list_services(project_id, region, application_name),
-            get_url_map_essentials(project_id),
+        availableUrlMaps = []
+        service_uris = apphub_app_list_services(
+            host_project_id,
+            project_id,
+            region,
+            application_name,
+            configuration,
+            secrets,
         )
+        url_map_essentials = get_url_map_essentials(
+            project_id, configuration, secrets
+        )
+
+        for details in url_map_essentials:
+            if details["service"] in service_uris:
+                availableUrlMaps.append(details["self_link"])
+                logger.debug(details["self_link"])
+
         for umap in availableUrlMaps:
             index = umap.find("/urlMaps/")
             if (
                 index != -1
                 and umap[index + len("/urlMaps/") :] == url_map_value
             ):
-                logger.info(
+                logger.debug(
                     "given url_map is found within the URL maps associated with the specified AppHub application"
                 )
                 return True  # Found, so return True immediately
 
     except Exception as e:
-        logger.error(f"Error retrieving URL map information: {e}")
-        return False
+        raise ActivityFailed(f"Error retrieving URL map information: {e}")
 
 
 def inject_fault_if_url_map_exists_app_hub(
     application_name: str,
+    host_project_id: str,
     url_map: str,
     project_id: str,
     region: str,
@@ -186,7 +211,7 @@ def inject_fault_if_url_map_exists_app_hub(
     regional: bool = True,
     configuration: Configuration = None,
     secrets: Secrets = None,
-) -> Dict[str, Any]:
+) -> None:
     """Processes a URL map if it exists within the specified AppHub application.
 
     Args:
@@ -203,12 +228,20 @@ def inject_fault_if_url_map_exists_app_hub(
         secrets: (Secrets) default None,
     """
 
-    if url_map_exists(url_map, project_id, region, application_name):
+    if url_map_exists(
+        url_map,
+        host_project_id,
+        project_id,
+        region,
+        application_name,
+        configuration,
+        secrets,
+    ):
         # URL map exists, proceed with your processing logic here
         logger.info(
-            f"URL map '{url_map}' found. Proceeding with Fault Injection Action "
+            f"Given URL map '{url_map}' found in given Apphub Application. Proceeding with Fault Injection Action "
         )
-        return inject_traffic_faults(
+        inject_traffic_faults(
             url_map,
             target_name,
             target_path,
@@ -221,12 +254,14 @@ def inject_fault_if_url_map_exists_app_hub(
             secrets,
         )
     else:
-        logger.info(f"URL map '{url_map}' not found, Can't Proceed Further")
-        return None
+        raise ActivityFailed(
+            f"Given URL map '{url_map}' not found in given Apphub Application, Cannot Inject Fault "
+        )
 
 
 def remove_fault_if_url_map_exists_app_hub(
     application_name: str,
+    host_project_id: str,
     url_map: str,
     project_id: str,
     region: str,
@@ -235,11 +270,12 @@ def remove_fault_if_url_map_exists_app_hub(
     regional: bool = True,
     configuration: Configuration = None,
     secrets: Secrets = None,
-) -> Dict[str, Any]:
+) -> None:
     """Processes a URL map if it exists within the specified AppHub application.
 
     Args:
         application_name (str): The name of the AppHub application.
+        host_project_id (str): The ID of the Host Apphub project.
         url_map (str): The URL map value to check for.
         project_id (str): The Google Cloud project ID.
         region (str): The region of the AppHub application.
@@ -250,12 +286,20 @@ def remove_fault_if_url_map_exists_app_hub(
         secrets: (Secrets) default None,
     """
 
-    if url_map_exists(url_map, project_id, region, application_name):
+    if url_map_exists(
+        url_map,
+        host_project_id,
+        project_id,
+        region,
+        application_name,
+        configuration,
+        secrets,
+    ):
         # URL map exists, proceed with your processing logic here
         logger.info(
-            f"URL map '{url_map}' found. Proceeding with Rollback of Fault Injection Action "
+            f"URL map '{url_map}' - Rollback of Fault Injection Action "
         )
-        return remove_fault_injection_traffic_policy(
+        remove_fault_injection_traffic_policy(
             url_map,
             target_name,
             target_path,
@@ -266,5 +310,6 @@ def remove_fault_if_url_map_exists_app_hub(
             secrets,
         )
     else:
-        logger.info(f"URL map '{url_map}' not found, Can't RollBack ")
-        return None
+        raise ActivityFailed(
+            f"Given URL map '{url_map}' not found, RollBack not required "
+        )
